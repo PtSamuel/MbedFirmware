169948ffb1a6147e43af60d80a890f297ee69e04
unknown <2695829662@qq.com>
2023-05-25 20:00:21 -0400

    Forward/Backward

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 #include <sstream>
 
 #define BUFSIZE 32
-#define FREQ_MULTIPLYER 24
+#define FREQ_MULTIPLYER 24.0f
+#define PWM_PERIOD_US 2000.0f
+#define ON_TIME_MIN_US 1100.0f
+#define ON_TIME_MAX_US 1900.0f
+#define ARM_ON_TIME 1500.0f
 
 AnalogIn ThrottleX(A0);
 AnalogIn ThrottleY(A1);
@@ -18,6 +22,13 @@ static BufferedSerial serial(USBTX, USBRX); // tx, rx
 static char buf[BUFSIZE];
 static char bufcpy[BUFSIZE];
 static size_t curlen = 0;
+
+float map_duty_cycle(float level) { // [-1.0, 1.0]
+    if (level > 1.0f || level < -1.0f) 
+        return ARM_ON_TIME / PWM_PERIOD_US;
+    float ON_TIME = ON_TIME_MIN_US + (level + 1) / 2 * (ON_TIME_MAX_US - ON_TIME_MIN_US);
+    return ON_TIME / PWM_PERIOD_US;
+}
 
 void parse(size_t len) {
     bufcpy[len - 1] = 0;
@@ -35,9 +46,8 @@ void parse(size_t len) {
 
     std::string inst; float level; 
     if(stream >> inst >> level) {
-        // printf("inst: %s, level: %f\n", inst.c_str(), level);
-        if(level > 1.0f || level < 0.0f)
-            printf("Level must be between 0 and 1\n");
+        if(level > 1.0f || level < -1.0f)
+            printf("Level must be between -1 and 1\n");
         else {
             if(inst == "$L1")
                 led1 = level;
@@ -48,9 +58,10 @@ void parse(size_t len) {
             else if(inst == "$L4")
                 led4 = level;
             else if(inst == "$LT")
-                throttle = level;
+                throttle = map_duty_cycle(level);
             else printf("Invalid instruction\n");
-            printf("current pulse width: %d us\n", throttle.read_pulsewidth_us() / FREQ_MULTIPLYER);
+
+            // printf("Current pulse width: %f us\n", throttle.read_pulsewidth_us() / FREQ_MULTIPLYER);
         }
     } else {
         printf("Invalid format\n");
@@ -71,9 +82,9 @@ int main()
     memset(buf, 0, BUFSIZE);
     printf("Started listening for command.\n");
     
-    throttle.period_us(2000);
-    float period_us = throttle.read_period_us() / FREQ_MULTIPLYER;
-    float period_s = period_us  * 1e-6;
+    throttle.period_us((int)PWM_PERIOD_US);
+    float period_us = (float)throttle.read_period_us() / FREQ_MULTIPLYER;
+    float period_s = period_us * 1e-6;
     printf("Throttle PWD frequency: %f Hz, period: %f us\n", 1 / period_s, period_us);
 
     while (1) {
@@ -84,55 +95,16 @@ int main()
         
 
         if(buf[curlen - 1] == '\r') {
-            // printf("new line detected\n");
-            // printf("\n"); // new line from buf content line
             std::memcpy(bufcpy, buf, curlen);
             parse(curlen);
             curlen = 0;
-        } else if(buf[curlen - 1] == 127) { // backspace
+        } else if(buf[curlen - 1] == 127) {
             if(curlen > 1)
                 curlen -= 2;
             else curlen = 0;
         }
 
-        printbuf();
+        // printbuf();
 
-        // size_t buflen = std::strlen(buf);
-        // if(buflen > 0)
-        //     printf("Receiving: %s\n", buf);
-        // ThisThread::sleep_for(100ms);
     }
 }
-
-/*
- * Copyright (c) 2006-2020 Arm Limited and affiliates.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// #include "mbed.h"
-
-// static BufferedSerial pc(USBTX, USBRX); // tx, rx
-// PwmOut led(LED1);
-
-// float brightness = 0.0;
-
-// int main()
-// {
-//     char msg[] = "Press 'u' to turn LED1 brightness up, 'd' to turn it down\n";
-//     char *c = new char[1];
-//     pc.write(msg, sizeof(msg));
-
-//     while (1) {
-//         pc.read(c, sizeof(c));
-//         pc.write(c, sizeof(c));
-//         if ((*c == 'u') && (brightness < 0.5)) {
-//             brightness += 0.01;
-//             led = brightness;
-//         }
-//         if ((*c == 'd') && (brightness > 0.0)) {
-//             brightness -= 0.01;
-//             led = brightness;
-//         }
-//     }
-
-// }
